68cd5b3b59327acd79778721c1da9692ce772838
mkhizebulelani94-sudo <mkhizebulelani94@gmail.com>
2026-04-20 21:05:58 +0200

    2nd commit

--- app/src/main/java/com/example/imaddraft2/MainActivity.kt
+++ app/src/main/java/com/example/imaddraft2/MainActivity.kt
@@ -13,19 +13,32 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.imaddraft2.ui.theme.IMADDraft2Theme
 
+
+
+
 class MainActivity : ComponentActivity() {
+    //Simple arrays
+    private val questions = arrayOf(
+        "T20 cricket is the most popular format in the world"
+        "Virat Kohli has 83 hundreds in all formats"
+        "Ab de Villiars has a T20 hundred"
+        "India is the most successful cricketing nation in history"
+        "The Proteas won the World Test Championship in 2025"
+    )
+
+    private val answers = arrayOf(
+        true, true, false, false, true
+    )
+
+    private var index = 0
+    private var score = 0
+    private var answered = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            IMADDraft2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_welcome)
+
     }
 }
+
